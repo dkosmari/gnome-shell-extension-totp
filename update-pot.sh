fac97e1ec612decdd4bd175b6d0c32fb73bae53c
Daniel K. O. (dkosmari) <none@none>
2024-02-26 13:35:58 -0300

    Minor tweaks.

--- update-pot.sh
+++ update-pot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
-exec xgettext --from-code=UTF-8 --output=po/totp@dkosmari.github.com.pot *.js
+exec xgettext \
+     --from-code=UTF-8 \
+     --copyright-holder='Daniel K. O.' \
+     --package-name='TOTP' \
+     --msgid-bugs='https://github.com/dkosmari/gnome-shell-extension-totp' \
+     --output=po/totp@dkosmari.github.com.pot \
+     *.js
