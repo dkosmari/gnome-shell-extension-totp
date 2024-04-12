af16e85191cfb95f8b76f242af08d56f6e0db6fc
Daniel K. O. (dkosmari) <none@none>
2024-04-12 10:50:34 -0300

    Forgot to add new file.

---
+++ mySpinRow.js
@@ -0,0 +1,70 @@
+/* mySpinRow.js
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
+// Implements most of Adw.SpinRow the extension needs.
+
+
+import Adw     from 'gi://Adw';
+import Gio     from 'gi://Gio';
+import GLib    from 'gi://GLib';
+import GObject from 'gi://GObject';
+import Gtk     from 'gi://Gtk';
+
+
+export default
+class SpinRow extends Adw.ActionRow {
+
+    static {
+        GObject.registerClass(
+            {
+                Properties: {
+                    'value': GObject.ParamSpec.double(
+                        'value',
+                        'Value',
+                        'The value',
+                        GObject.ParamFlags.READWRITE,
+                        -Number.MAX_VALUE,
+                        Number.MAX_VALUE,
+                        0
+                    )
+                }
+            },
+            this);
+    }
+
+
+    constructor({adjustment = null,
+                 climb_rate = null,
+                 digits = null,
+                 numeric = null,
+                 snap_to_ticks = null,
+                 update_policy = null,
+                 wrap = null,
+                 value = null,
+                 ...args})
+    {
+        super(args);
+
+        const spin = new Gtk.SpinButton({
+            adjustment,
+            climb_rate,
+            digits,
+            numeric,
+            snap_to_ticks,
+            update_policy,
+            wrap,
+            value,
+        });
+
+        this.bind_property('value',
+                           spin.adjustment, 'value',
+                           GObject.BindingFlags.BIDIRECTIONAL);
+
+        this.add_suffix(spin);
+    }
+
+
+};
