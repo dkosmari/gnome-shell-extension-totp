d973f61ec07929b2094197efa5cf88a514d7b7a0
Daniel K. O. (dkosmari) <none@none>
2025-12-23 18:06:43 -0300

    Remove SHA-384 to avoid breaking older systems.
    --HG--
    branch : g-44

--- otp.js
+++ otp.js
@@ -31,9 +31,11 @@ class Algorithm {
         case 'SHA256':
         case 'SHA-256':
             return GLib.ChecksumType.SHA256;
+/*
         case 'SHA384':
         case 'SHA-384':
             return GLib.ChecksumType.SHA384;
+*/
         case 'SHA512':
         case 'SHA-512':
             return GLib.ChecksumType.SHA512;
